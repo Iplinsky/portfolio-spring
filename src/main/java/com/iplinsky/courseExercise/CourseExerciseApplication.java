fd3c2e27e6a1d66fd6e52e38dabd1bf074856dc5
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2021-02-28 16:36:40 -0300

    Project created

---
+++ src/main/java/com/iplinsky/courseExercise/CourseExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.iplinsky.courseExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iplinsky/courseExercise/CourseExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iplinsky.courseExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
